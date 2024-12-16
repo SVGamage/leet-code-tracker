b816c97b0f0ea50430bf82aa3865c71b46ad7752
SVGamage <119811762+SVGamage@users.noreply.github.com>
2024-12-17 02:42:19 +0530

    fix a minor issue

--- components/category-icon.tsx
+++ components/category-icon.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as Icons from "lucide-react";
+import { ElementType } from "react";
 
 interface CategoryIconProps {
   name: string;
@@ -8,6 +9,6 @@ interface CategoryIconProps {
 }
 
 export function CategoryIcon({ name, className }: CategoryIconProps) {
-  const Icon = Icons[name as keyof typeof Icons];
+  const Icon = Icons[name as keyof typeof Icons] as ElementType;
   return Icon ? <Icon className={className} /> : null;
 }
